02148586bd05eb2a0a4fba66fd7f51cb2851a3c1
Michael Thelin <thelinmichael@gmail.com>
2024-01-19 14:17:49 +0100

    Fix debugger key shortcuts

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -5,9 +5,19 @@
 return {
   {
     "mfussenegger/nvim-dap",
+    lazy = false,
     dependencies = {
       "rcarriga/nvim-dap-ui",
       "leoluz/nvim-dap-go",
+    },
+    keys = {
+      {
+        "<leader>dd",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Debug this shit (or continue)",
+      },
     },
     config = function()
       local dapui = require("dapui")
@@ -26,39 +36,6 @@ return {
       end
 
       -- nvim-dap keymappings
-      map = vim.keymap.set
-      -- Press f5 to debug
-      map("n", "<leader>dd", [[:lua require'dap'.continue()<CR>]], { desc = "Debug menu/Continue" })
-      -- Press CTRL + b to toggle regular breakpoint
-      map("n", "<leader>db", [[:lua require'dap'.toggle_breakpoint()<CR>]], { desc = "Toggle breakpoint" })
-      -- Press CTRL + c to toggle Breakpoint with Condition
-      map(
-        "n",
-        "<leader>dc",
-        [[:lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint Condition: '))<CR>]],
-        { desc = "Breakpoint condition" }
-      )
-      -- Press CTRL + l to toggle Logpoint
-      map(
-        "n",
-        "<leader>dp",
-        [[:lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log Point Msg: '))<CR>]],
-        { desc = "Breakpoint log" }
-      )
-      -- Pressing F10 to step over
-      map("n", "<leader>dso", [[:lua require'dap'.step_over()<CR>]], { desc = "Step over " })
-      -- Pressing F11 to step into
-      map("n", "<leader>dsi", [[:lua require'dap'.step_into()<CR>]], { desc = "Step into" })
-      -- Pressing F12 to step out
-      map("n", "<leader>dsx", [[:lua require'dap'.step_out()<CR>]], { desc = "Step out" })
-      -- Press F6 to open REPL
-      map("n", "<leader>dor", [[:lua require'dap'.repl.open()<CR>]], { desc = "Open REPL" })
-      -- Press dl to run last ran configuration (if you used f5 before it will re run it etc)
-      map("n", "<leader>dl", [[:lua require'dap'.run_last()<CR>]], { desc = "Debug last" })
-
-      -- Press Ctrl+d to toggle debug mode
-      -- TODO: Remove NeoTree
-      map("n", "<leader>dt", [[:lua require'dapui'.toggle()<CR>]], { desc = "Toggle debug" })
       require("dap-go").setup({
         dap_configurations = {
           {
